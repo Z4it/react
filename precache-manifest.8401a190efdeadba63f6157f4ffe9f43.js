bb744af8e7fd553921c8dd219192b4dbb411826e
Mikhail Martynov <xbox.777@mail.ru>
2020-06-11 10:31:35 +0300

    first-test-site

---
+++ precache-manifest.8401a190efdeadba63f6157f4ffe9f43.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78de62926ff9a0dee8065d86ee5b33a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6f8e78cc06aef2ded3d",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "f8bc5e012aacb557425c",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d6f8e78cc06aef2ded3d",
+    "url": "/static/js/2.f1e5dd85.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f1e5dd85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8bc5e012aacb557425c",
+    "url": "/static/js/main.e9537125.chunk.js"
+  },
+  {
+    "revision": "735797d51b2e69cf5592",
+    "url": "/static/js/runtime-main.d0498219.js"
+  },
+  {
+    "revision": "43bb6970fc2519399172818e5ff0bd85",
+    "url": "/static/media/carrot.43bb6970.jpg"
+  },
+  {
+    "revision": "a3fb03ad31399d746602762fbf7066c9",
+    "url": "/static/media/green.a3fb03ad.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
